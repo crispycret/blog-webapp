50cc4a15ac16b4bf2b18c17c2a9df103c294e699
crispycret <bnadeau.dev@gmail.com>
2022-10-30 05:16:38 -0400

    Removed unused navigation titles and edited color shceme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,69 +4,20 @@ import { useEffect, useState } from "react";
 import { Container, Card } from 'react-bootstrap'
 
 import { Props } from "../App";
-import { Post } from "../components/Post/Post";
+import PostList from "../components/Post/PostList";
 
 
 export const Home = (props: Props) => {
 
-    // Create file PostManager to manage retireving posts
-
-    // For now ?
-
-    const [postsData, setPostsData] = useState<Array<object>>([])
-    const [posts, setPosts] = useState<JSX.Element[]>([<></>])
-
-    const getPosts = async () => {
-        
-        let res = await props.api.client.get('/post/all')
-        
-        .catch((error: AxiosError) => {
-            return Promise.reject(error);
-        })
-
-        if (res.data.status == 200) {
-            // Store raw posts data ??
-            setPostsData(res.data.body.posts)
-            
-            // For each post object in the request create a post element.
-            let elements = []
-            for (let post of res.data.body.posts) {
-                let element = <>
-                    <Container className='mb-3'>
-                        <Card>
-                            <Post {...props} title={post.title} body={post.body} bodyLimit={500}/>            
-                        </Card>
-                    </Container>
-                </>
-                elements.push(element)
-            }
-
-            setPosts(elements)
-        }
-
-        return res
-    }
-
-
-    useEffect(() => {
-        getPosts()
-    }, [])
-
-
     return (
         <>
+            <Container className='bg-dark text-white'>
+                <div className='mb-3'></div>
 
-            <div className='mb-3'></div>
+                <PostList {...props} />
 
-            {posts}
-
-            {!props.user.active &&
-                <>
-                {
-
-                }
-                </>
-            }
+                { !props.user.active && <>{}</> }
+            </Container>
 
         </>
     )
